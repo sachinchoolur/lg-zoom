92f70f248508ac4abec5ea793a0fc216000712c5
Sachin Neravath <sachi77n@gmail.com>
2020-10-05 13:50:11 +0530

    Update banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= grunt.template.today("mmmm-dd-yyyy") %>\n' +
             '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
             ' Licensed GPLv3 */\n',
